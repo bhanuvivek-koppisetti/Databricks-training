427892565cb397add021e73d0670c31486a43c43
Koppisetti Bhanu vivek <koppisettibhanuvivek@gmail.com>
2026-05-07 11:22:04 +0530

    Add various SQL join queries for employee and department

--- Week1/Day1/Queries.sql
+++ Week1/Day1/Queries.sql
@@ -190,6 +190,68 @@ on Department.department_id =  Employee.department_id
 GROUP BY Department.department_id
 ORDER BY total_salary;
 
+------------------------------------------------------------------------------
+--Join Queries
+------------------------------------------------------------------------------
+
+-- Select employee names along with their department names
+SELECT Employee.name as emp_name,Department.name as dept_name FROM Employee
+JOIN Department
+ON Employee.department_id = Department.department_id;
+
+-- Select project names along with the department names they belong to
+SELECT Project.name as Project_name,Department.name as dept_name From Project
+JOIN Department
+ON Project.department_id = Department.department_id;
+
+-- Select employee names and their corresponding project names
+SELECT Employee.name as emp_name,Project.name as Project_name FROM Employee
+JOIN Project
+ON Employee.department_id = Project.department_id;
+
+-- Select all Employees and their departments including those wothout a department
+SELECT Employee.name as emp_name, Department.name as dept_name From Employee
+LEFT JOIN Department
+ON Employee.department_id = Department.department_id;
+
+-- Select all departments and their employees, including departments without employees
+SELECT Department.name as dept_name, Employee.name as emp_name FROM Department
+LEFT JOIN Employee
+ON Department.department_id = Employee.department_id;
+
+-- Select employees who are not assigned to any project
+SELECT Employee.name as emp_name, Project.name as project_name
+FROM Employee LEFT JOIN Project
+ON Employee.department_id = Project.department_id
+WHERE Project.project_id is NULL;
+
+-- Select employees and the number of projects their department is working on.
+SELECT Employee.name as emp_name,Project.department_id as dept_id,COUNT(Project.Project_id) as total_projects FROM Employee
+JOIN Project 
+ON Employee.department_id = Project.department_id
+GROUP BY Employee.name,Project.department_id;
+
+-- Select the departments that have no employees
+Select Department.department_id as dept_id,Department.name as dept_name,Employee.emp_id as emp_id From Department
+LEFT JOIN Employee
+ON Department.department_id = Employee.department_id
+WHERE Employee.emp_id is NULL ;
+
+-- Select employee names who share the same department with 'John Doe'
+SELECT Employee.name as emp_name From Employee 
+Where Employee.department_id = (
+  SELECT Employee.department_id From Employee 
+  WHERE Employee.name = 'John Doe'
+) 
+And Employee.name <> 'John Doe';
+
+-- Select the department name with the highest average salary
+SELECT Department.name as dept_name,AVG(Employee.salary) as average_salary FROM Department
+RIGHT JOIN Employee 
+ON Department.department_id = Employee.department_id
+GROUP BY Department.name
+ORDER BY average_salary DESC
+LIMIT 1;
 
 
 
